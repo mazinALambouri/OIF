203e37bf83a18fa36299a6a4b8c8ae71e8d89d8c
OmarNasse <96281102+OmarNasse@users.noreply.github.com>
2025-03-23 16:26:30 +0400

    done

--- oifsite/src/components/Navigation.js
+++ oifsite/src/components/Navigation.js
@@ -364,9 +364,7 @@ const Navigation = () => {
                     }}
                     whileTap={{ scale: 0.92, y: 0 }}
                   >
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
-                    </svg>
+                    DOWNLOAD GUIDE
                   </motion.button>
                 </motion.li>
               </div>
@@ -525,9 +523,7 @@ const Navigation = () => {
                   flex items-center justify-center
                   mx-auto"
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
-                  </svg>
+                  DOWNLOAD GUIDE
                 </button>
               </motion.li>
             </motion.ul>
