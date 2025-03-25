6f23877cba9a9beecc85056e975d7e669416fa02
OmarNasse <96281102+OmarNasse@users.noreply.github.com>
2025-03-25 15:47:02 +0400

    Done

--- oifsite/src/components/Speakers.js
+++ oifsite/src/components/Speakers.js
@@ -49,7 +49,7 @@ const Speakers = () => {
         const { data, error } = await supabase
           .from('speakers')
           .select('id, name, position, company, gender, img, description')
-          .order('id');
+          .order('id', { ascending: true });
           
         if (error) {
           throw error;
